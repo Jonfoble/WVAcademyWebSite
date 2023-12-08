be6abbef11982b82fef4eb1cf6fb58477752f24e
Emre Guzel <91648268+Jonfoble@users.noreply.github.com>
2023-12-08 20:47:26 +0300

    Update

---
+++ WVAcademyWebSite/completed.js
@@ -0,0 +1,13 @@
+
+function done1(){
+ document.getElementById("1").innerHTML = "DONE";
+
+}
+function done2(){
+    document.getElementById("2").innerHTML = "DONE";
+}
+
+function done3(){
+    document.getElementById("3").innerHTML = "DONE";
+   
+}
